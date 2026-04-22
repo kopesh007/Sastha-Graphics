599f4b6a315fd7caccc98282aa3771b3b975e644
Kopesh <kopesh@example.com>
2026-04-22 11:45:38 +0530

    fronend almost ready

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,8 @@ app_name='website'
 
 urlpatterns=[
     path("",views.home,name="home"),
+    path("about/",views.about,name="about"),
+    path("banner/",views.banner_page,name="banner"),
+    path("invite/",views.invites,name="invitations"),
+    path("art/",views.arts,name="arts"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 def home(request):
     return render(request,"index.html")
 
+def about(request):
+    return render(request,"about.html")
+
+def banner_page(request):
+    return render(request,"banner.html")
+
+def invites(request):
+    return render(request, "invitation.html")
+
+def arts(request):
+    return render(request,"art.html")
 # Create your views here.
